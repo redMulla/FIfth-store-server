611faae24787b7db5994859ac5e964f8c0c02727
redMulla <ivassillycharite@gmail.com>
2024-08-03 10:23:09 +0200

    auth login setup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from '../user/user.module';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
-@Module({})
+@Module({
+  imports: [UserModule],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async signIn(email: string, pass: string) {
+    const user = await this.userService.getUserByMail(email);
+
+    if (!pass) {
+      throw new BadRequestException('Password is required');
+    }
+
+    const isMatch = await bcrypt.compare(pass, user.password);
+    if (!isMatch) {
+      throw new UnauthorizedException('Wrong credentials');
+    }
+
+    const { password, ...result } = user.toJSON();
+    return result;
+  }
+}
